532275c650747b2d6845966153b2a12350f95bbc
Jordan_Wills <jordanewills@gmail.com>
2023-06-11 20:08:41 +0100

    Readme and removing redundant stuff

--- NVAPIIlluminationControlService/x64/Debug/installservice.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d "%~dp0"
-
-set "sourceFile=NVAPIIlluminationControlService.exe"
-set "destinationDirectory=C:\NvAPIIlluminationController"
-copy "%sourceFile%" "%destinationDirectory%"
-
-start "" "NVAPIIlluminationControlService.exe" install
